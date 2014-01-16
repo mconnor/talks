b3224291b7d5a6fabc3852a342f26c4cd1414810
sharond <sdiorio@euro-pro.local>
2014-01-15 23:32:08 -0700

    re-ordered stuff

--- 2014/ng-conf-filters/demos/magical-demo/app/scripts/controllers/main.js
+++ 2014/ng-conf-filters/demos/magical-demo/app/scripts/controllers/main.js
@@ -9,12 +9,13 @@ angular.module('magicalDemoApp')
     var d3MakeObject = $filter('d3MakeObject');
 
     $scope.filterItems = function (filterBy) {
+        console.log("items: " + $scope.items.length)
         $scope.filteredObject = d3MakeObject($scope.items, filterBy);
     }
 
     $scope.getData = function() {
         $scope.status = "finding...";
-        $http.get('http://localhost:3000/api/v1/dns?limit=10000').then(function (results) {
+        $http.get('http://localhost:3000/api/v1/dns').then(function (results) {
             $scope.status = "ready.";
             $scope.items = results.data;
             $scope.filterItems($scope.filterOn);
