5bc16bfe3bb915fdf49cec6a55bcaeea4274bae8
sharond <sdiorio@euro-pro.local>
2014-01-15 21:28:59 -0700

    Editing

--- 2014/ng-conf-filters/demos/magical-demo/app/scripts/controllers/main.js
+++ 2014/ng-conf-filters/demos/magical-demo/app/scripts/controllers/main.js
@@ -4,21 +4,22 @@ angular.module('magicalDemoApp')
 .controller('MainCtrl', function ($scope, $http, $filter) {
 
     $scope.status = "waiting...";
+    $scope.filterOn = "TUCOW";
     $scope.filteredObject = {};
     var d3MakeObject = $filter('d3MakeObject');
 
     $scope.filterItems = function (filterBy) {
-        $scope.filteredObject = d3MakeObject($scope.items, filterBy)
+        $scope.filteredObject = d3MakeObject($scope.items, filterBy);
     }
 
     $scope.getData = function() {
         $scope.status = "finding...";
         $http.get('http://localhost:3000/api/v1/dns?limit=10000').then(function (results) {
-            $scope.status = "ready." 
+            $scope.status = "ready.";
             $scope.items = results.data;
-            $scope.filterItems('arin')
-        })
+            $scope.filterItems($scope.filterOn);
+        });
     }
-    $scope.getData()
+    $scope.getData();
 
 });
